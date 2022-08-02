818a33984922a64ac3377a63e726038450a21ac7
Merielly Santana <santana.meri10gmail.com>
2022-08-02 14:48:30 -0300

    Blog Pessoal

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @Entity
 @Table(name = "postagens")
@@ -20,14 +23,22 @@ public class Postagem {
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private long id;
+	
 	@NotNull
 	@Size(min=5, max=100)
 	private String titulo;
+	
 	@NotNull
 	@Size(min=2, max=500)
 	private String texto;
+	
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+	
+	@ManyToOne
+	@JsonIgnoreProperties ("postagem")
+	private TemaModel tema;
+	
 	public long getId() {
 		return id;
 	}
@@ -51,6 +62,12 @@ public class Postagem {
 	}
 	public void setDate(Date date) {
 		this.date = date;
+	}
+	public TemaModel getTema() {
+		return tema;
+	}
+	public void setTema(TemaModel tema) {
+		this.tema = tema;
 	}
 	
 	
