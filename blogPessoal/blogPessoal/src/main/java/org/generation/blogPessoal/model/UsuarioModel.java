ffd82974934f6ab51401a6af7d843ff13a33b554
Merielly Santana <santana.meri10gmail.com>
2022-08-18 14:08:31 -0300

    Atualização do Blog Pessoal

--- blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/UsuarioModel.java
+++ blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/UsuarioModel.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table(name = "tb_usuarios")
 public class UsuarioModel {
@@ -26,6 +28,7 @@ public class UsuarioModel {
 	@NotNull(message = "O atributo nome é obrigatório!")
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O atributo usuário é obrigatório!")
 	@Email(message = "O atributo usuário deve ser um email válido!")
 	private String usuario;
